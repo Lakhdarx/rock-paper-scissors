74a25dc35b01392d261d25e38475f1383a967189
Lakhdar <Lakhdarbhl76@gmail.com>
2024-06-28 19:40:08 +0100

    Create playRound function that determines a winner in each round, and increments scores

--- js/main.js
+++ js/main.js
@@ -13,3 +13,28 @@ function getHumanChoice() {
     let choice = prompt('Rock, paper or scissor?');
     return choice;
 }
+
+
+function playRound(humanChoice,computerChoice) {
+// Make humanChoice case-insensitive    
+    let humanLower = humanChoice.toLowerCase();
+    let firstChar = humanChoice[0].toUpperCase();
+    humanChoice = firstChar + humanLower.substr(1);
+
+// Logic of the game
+    if (humanChoice === computerChoice) console.log('Tie!');
+    else if (humanChoice === 'Rock' && computerChoice === 'Paper' || humanChoice === 'Paper' && computerChoice === 'Scissor' || humanChoice === 'Scissor' && computerChoice === 'Rock') {
+        ++computerScore;
+        console.log(`You lose, ${computerChoice} beats ${humanChoice}`);
+    }
+    else {
+        ++humanScore;
+        console.log(`You win, ${humanChoice} beats ${computerChoice}`);
+    }
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
