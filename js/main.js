e0c17842eae876f50b1d5092aeee64a77370a424
Lakhdar <Lakhdarbhl76@gmail.com>
2024-06-28 19:13:43 +0100

    Create getComputerChoice function to get a random input from computer

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function getComputerChoice() {
+    let x = Math.floor(Math.random() * 3)+1;
+    if (x === 1) return('Rock');
+    else if (x === 2) return('Paper');
+    else return('Scissor');
+}
+
+
